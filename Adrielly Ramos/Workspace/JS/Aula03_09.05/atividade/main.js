bc89242d75784b40873cfc9d2efb460c0d48a637
AdriellyN <rrpinheiro014@gmail.com>
2023-05-09 21:42:15 -0300

    Atividade 03 -  JS

---
+++ Adrielly Ramos/Workspace/JS/Aula03_09.05/atividade/main.js
@@ -0,0 +1,29 @@
+var fruta1 = " banana"
+var fruta2 = "Maçã"
+var fruta3 = "Abacate"
+var fruta4 = "Melão"
+var fruta5 = "Laranja"
+
+
+console.log("Fruta 01: " + fruta1 + " - "  + fruta1.length + " caracteres");
+console.log("Fruta 02: " + fruta2 + " - "  + fruta2.length + " - caracteres");
+console.log("Fruta 03: " + fruta3 + " - "  + fruta3.length + " - caracteres");
+console.log("Fruta 04: " + fruta4 + " - "  + fruta4.length + " - caracteres");
+console.log("Fruta 05: " + fruta5 + " - "  + fruta5.length + " - caracteres");
+
+console.log(fruta1.substring(1, 4));
+console.log(fruta2.substring(0, 3));
+console.log(fruta3.substring(0, 3));
+console.log(fruta4.substring(0, 3));
+console.log(fruta5.substring(0, 3));
+
+console.log(fruta1.trim());
+console.log(fruta2.toLowerCase());
+console.log(fruta3.toLowerCase());
+console.log(fruta4.toLowerCase());
+console.log(fruta5.toLowerCase());
+
+let fruta1rep
+
+console.log(fruta1 + " " + fruta2 + " " + fruta3 + " " + fruta4 + " " + fruta5)
+

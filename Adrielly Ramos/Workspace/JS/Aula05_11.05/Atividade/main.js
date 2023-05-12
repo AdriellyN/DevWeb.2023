856ecf69d512cecc9800ccf8a714f2a75a151819
AdriellyN <adriellynr@gmail.com>
2023-05-11 21:29:40 -0300

    Aula 05 - JS

---
+++ Adrielly Ramos/Workspace/JS/Aula05_11.05/Atividade/main.js
@@ -0,0 +1,15 @@
+// while
+// let contador = 1;
+// while (contador <= 50) {
+//     console.log(`Valor = ${contador}`);
+//     contador++;
+// }
+
+// do while
+// console.clear();
+let i = 10 ;
+do {
+    console.log(`Valor = ${i}`);
+    i--;
+} while (i > 0);
+console.log(i);

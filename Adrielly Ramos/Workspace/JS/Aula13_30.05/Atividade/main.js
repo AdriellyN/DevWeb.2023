a296fc53b9100f97cb7fd884006a2d914ad4335b
AdriellyN <adriellynr@gmail.com>
2023-05-30 21:38:35 -0300

    Aula 13

---
+++ Adrielly Ramos/Workspace/JS/Aula13_30.05/Atividade/main.js
@@ -0,0 +1,9 @@
+let titulo = document.getElementById('titulo');
+
+titulo.style.textAlign = 'center';
+titulo.style.backgroundColor = '#CDC1C9';
+titulo.style.borderBottom = 'solid 3px #000';
+titulo.style.margin = '20px';
+
+let ul = document.getElementById('list');
+ul.style.listStyle = 'none';
